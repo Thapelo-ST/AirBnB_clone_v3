82fad37d8bf50095d94d297c30070736af40a637
root <root@Motherfuquer>
2023-10-29 08:14:15 +0200

    fix 1

--- api/v1/app.py
+++ api/v1/app.py
@@ -36,3 +36,9 @@ def not_found(error):
     response = {'error': 'Not found'}
     return jsonify(response), 404
 
+
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_HOST', '5000'))
+    app.run(host=HOST, port=PORT, threaded=True)
+

2105dde99c4ebee0ba2cf9670e91aa5ded611858
Demakatso <demakatsomay@gmail.com>
2023-10-30 13:37:48 -0700

    fix2

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,44 +1,36 @@
 #!/usr/bin/python3
-"""
-    creates flask app(HBNB) and registers the BluePrint app_views with flask instance HBNB
-"""
+'''Contains a Flask web application API.'''
 
-from os import getenv
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
 
-app = flask(__name__)
 
-# allow request from any origin and enable cors
-CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
-# register app_views blueprint
-app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
 
 
-# teardown function for SQLAlchemy close session after each request
 @app.teardown_appcontext
-def engine_teardown(exception):
-    """
-        delete current SQLAlchemy session obj after each request
-    """
+def downtear(self):
+    '''Status of your API'''
     storage.close()
 
-# Errot handling
+
 @app.errorhandler(404)
-def not_found(error):
-    """
-        return "not found" erorr message from json response
-    """
-    response = {'error': 'Not found'}
-    return jsonify(response), 404
+def error_404(error):
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
-
-if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_HOST', '5000'))
-    app.run(host=HOST, port=PORT, threaded=True)
-
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)

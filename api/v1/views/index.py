7e0bf5d1ad8b8d822454c7d60591469747764f57
root <root@Motherfuquer>
2023-10-30 10:54:24 +0200

    fix 2

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-    make a route /status on obj app_views
-"""
+"""make a route /status on obj app_views"""
 
 from flask import jsonify
 from api.v1.view import app_view
@@ -10,9 +8,7 @@ from models import storage
 
 @app_views.route('/status', method=['GET'])
 def api_status():
-    """
-        returns json for RestFul API status
-    """
+    """returns json for RestFul API status"""
     resp = {'status': 'OK'}
     return jsonify(resp)
 
@@ -20,9 +16,7 @@ def api_status():
 # Task Number 4
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
-    """
-        gets number of objects in each of the type
-    """
+    """gets number of objects in each of the type"""
     stats = {
             'amenities': storage.count('Amenity'),
             'cities': storage.count('City'),
